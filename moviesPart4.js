b928b667bd86b5ca67c72380ed4011e86be9e9ca
Wayra <wayrasoft10@gmail.com>
2025-04-14 14:51:51 +1000

    New Branch created.
    index.html created and moviesPart4.js created (it contains a Class called Movie, its constructor and properties).

---
+++ moviesPart4.js
@@ -0,0 +1,15 @@
+// File: moviesPart.js
+// This file shows a Class and its properties and methods.
+// Author: Carlos
+// Date: 14/04/2025
+
+// Q4.1 Define a movie class, its constructor and properties:
+class Movie {
+  constructor(movieId, title, year, rating) {
+    this.movieId = movieId;
+    this.title = title;
+    this.year = year;
+    this.rating = rating;
+  }
+}
+console.log(Movie);
